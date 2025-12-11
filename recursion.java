5d7120bad84cf10ac81b33a8dc31acaf0a4b1f82
Ayush Gupta <ag6320226@gmail.com>
2025-12-11 21:11:58 +0530

    recursion

---
+++ recursion.java
@@ -0,0 +1,75 @@
+public class recursion {
+    // public static void printNumb(int n){
+//         if(n==0){
+//             return ;
+//     }
+//         System.out.println(n);
+//         printNumb(n-1);
+
+//     }
+//     public static void main(String[] args) {
+//         int n=5;
+//         printNumb(n);
+//     }
+// }
+//   if(n==6){
+//             return ;
+//     }
+//         System.out.println(n);
+//         printNumb(n+1);
+
+//     }
+//     public static void main(String[] args) {
+//         int n=1;
+//         printNumb(n);
+//     }
+// }
+    //      public static void printNumb(int n,int sum,int i){
+    //         if(i==n){
+    //             sum +=i;
+    //             System.out.println(sum);
+    //             return ;
+
+    //         }
+    //         sum +=i;
+    //         printNumb(i+1, sum, n);
+    //     }
+    //     public static void main(String[] args) {
+    //         printNumb(1 , 5, 0);
+    //     }
+    // }
+
+    // print factorial of a number
+//     public static int calcfactorial(int n){
+//         if(n==1|| n==0){
+//             return 1;
+//         }
+//         int fact_n_minus_1 =calcfactorial(n-1);
+//         int fact_n=n*fact_n_minus_1;
+//         return fact_n;
+//     }
+//     public static void main(String[] args) {
+//   int n=7;
+//      int ans= calcfactorial(n);
+//      System.out.println(ans);
+// }
+// }
+// public static void printfib(int a,int b,int n){
+//     if(n==0){
+//         return ;
+//     }
+//     int c=a+b;
+//     System.out.println(c);
+//     printfib(b, c, n-1);
+
+
+// }
+// public static void main(String[] args) {
+    
+//     int a=0,b=1;
+//     System.out.println(a);
+//     System.out.println(b);
+//     int n=9;
+//     printfib(a, b, n-2);
+// }
+// }
