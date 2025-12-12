35cc4847be0c408d67180c7ad32c6099fe2168bd
Ayush Gupta <ag6320226@gmail.com>
2025-12-12 09:30:14 +0530

    string

---
+++ Strings.java
@@ -0,0 +1,64 @@
+// import java.util.Scanner;
+
+// public class Strings {
+//     public static void main(String[] args) {
+// //         Scanner sc= new Scanner(System.in);
+//         String name= sc.nextLine();
+//         System.out.println("your name is:"+ name);
+        
+//     }
+    
+// // }
+// String firstname= "Babulal";
+// String lastname="Bhai";
+// String fullname= firstname  +"@"+ lastname;
+// System.out.println(fullname.length());
+
+// for(int i=0;i<fullname.length();i++){
+//     System.out.println(fullname.charAt(i));
+
+
+//     String name1="ayush";
+//     String name2="ayush3";
+
+//     if(name1==name2){
+//         System.out.println("string are equal:");
+
+//     }else{
+//         System.out.println("string are not equal:");
+//     }
+// }
+// }
+// StringBuilder sb=new StringBuilder("hello");
+// System.out.println(sb);
+
+// // System.out.println(sb.charAt(0));
+// // sb.setCharAt(4,'i');
+
+// // System.out.println(sb);
+// //     }
+// // }
+// // insersion
+// sb.insert(1,'m');
+// sb.insert(4,'a');
+// System.out.println(sb);
+
+// sb.delete(2,3);
+// System.out.println(sb);
+//     }
+// }
+// // reverse string
+// for(int i=0;i<sb.length()/2;i++){
+//     int front=i;
+//     int back = sb.length()-1-i;
+
+//     char frontChar= sb.charAt(front);
+//     char backChar=sb.charAt(back);
+  
+//     sb.setCharAt(front, backChar);
+//     sb.setCharAt(back, frontChar);
+// }
+// System.out.println(sb);
+//     }
+// }
+
