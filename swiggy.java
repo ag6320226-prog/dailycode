9494ac1a76b60fd9e2862f91a7d9b078ba8bb57c
Ayush Gupta <ag6320226@gmail.com>
2025-12-14 22:43:31 +0530

    miniprogram

---
+++ swiggy.java
@@ -0,0 +1,59 @@
+// class swiggyApp{
+//     private String username;
+//     private String address;
+//     private String fooditem;
+//       private int price;
+
+               
+//               public  String getusername(){
+//                 return username;
+//                }
+//                public void setusername(String name){
+//                 username=name;
+//         System.out.println("uername:"+ getusername());
+//                }
+//                public String getaddress(){
+//                 return address;
+
+//                }
+//                public void setaddress(String a){
+//                         address=a;
+//                     System.out.println("Address:"+ getaddress());
+
+//                }
+
+//                         public String getfooditem(){
+//                             return fooditem;
+
+//                         }
+//                           public void setfooditem(String f){
+//                             fooditem= f;
+//                             System.out.println("fooditem:"+ getfooditem());
+
+//                           }
+//                                 public int getprice(){
+//                                     return price;
+//                                 }
+//                                 public void setprice(int p){
+//                                     price=p;
+//                                        System.out.println("price:"+ getprice());
+//                                 }
+//                                 public void placeorder(){
+//                                     System.out.println("order confirmed");
+//                                 }
+
+                   
+
+               
+
+// }
+//       public class swiggy{
+//         public static void main(String[] args) {
+//             swiggyApp s1=new swiggyApp();
+//             s1.setusername("prince babul");
+//             s1.setaddress("gandhigram");
+//             s1.setfooditem("dalMukhni");
+//             s1.setprice(459);
+//             s1.placeorder();
+//         }
+//       }
