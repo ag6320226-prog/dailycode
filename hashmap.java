5837dc59477ea280ce7517cdb55beb35bb846554
Ayush Gupta <ag6320226@gmail.com>
2025-12-15 22:28:32 +0530

    map

---
+++ hashmap.java
@@ -0,0 +1,58 @@
+// import java.util.HashMap;
+// import java.util.Map;
+// import java.util.Set;
+
+// public class hashmap {
+//     public static void main(String[] args) {
+//         HashMap<String,Integer> map=new HashMap<>();
+
+// // //insertion
+//       map.put("india", 130);
+//       map.put("US", 30);
+//       map.put("china", 150);
+//     //   System.out.println(map);
+
+
+//       map.put("india", 140);
+//     //   System.out.println(map);
+//       map.put("us", 50);
+//     //   System.out.println(map);
+// // //     }  
+// // // }
+
+
+// // // //
+// // // // //search
+// // // // if(map.containsKey("southafrica")){
+// // // //     System.out.println("key is presented in the map");
+
+// // // // }else{
+// // // //     System.out.println("key is not presented in the map");
+// // // // }
+// // // System.out.println(map.get("india"));
+// // // System.out.println(map.get("southafrica"));
+// // //     }
+// // // }
+// // int arr[]={14,35,45,67,64};
+// // for(int i=0;i<5;i++){
+// //     System.out.print(arr[i]+"");
+// // }
+// // System.out.println();
+// // for(int val: arr){
+// //     System.out.println(val + " ");
+// // }
+// // System.out.println();
+// //     }
+// // }
+
+// for(Map.Entry<String,Integer> e: map.entrySet()){
+//     System.out.println(e.getKey());
+//     System.out.println(e.getValue());
+
+// }
+// Set<String> keys = map.keySet();
+// for(String key: keys){
+//     System.out.println(key+ " " + map.get(keys));
+// }
+//     }
+// }
