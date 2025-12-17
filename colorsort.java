ea9167fc61544ac28ac1c07140fe07462e288e17
Ayush Gupta <ag6320226@gmail.com>
2025-12-17 21:52:28 +0530

    arr

---
+++ colorsort.java
@@ -0,0 +1,30 @@
+// public class colorsort {
+// public static void main(String[] args) {
+    
+
+        
+//     int arr[]={1,3,6,2,0,5};
+
+//     int low=0;
+//     int mid=0;
+// int h=arr.length-1;
+// while(mid<=h){
+//     if(arr[mid]==0){
+//         swap [arr,low,mid];
+//         low++;
+//         mid++;
+
+//     }else if(arr[mid]==1){
+//         mid++;
+
+
+//     }else{
+//         swap(arr,mid,low);
+//         h--;
+//     }
+//     }
+// }
+    
+
+    
+// }
