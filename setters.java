e638fce851ee5d0cd6a9ba3c19ff2063bd9d1ee1
Ayush Gupta <ag6320226@gmail.com>
2025-12-18 22:58:02 +0530

    arraylis

---
+++ setters.java
@@ -0,0 +1,67 @@
+
+// import java.util.ArrayList;
+// import java.util.LinkedList;
+
+// public class setters {
+//     public static void main(String[] args) {
+// // //         ArrayList<Integer>list =new ArrayList<>();
+// // //         ArrayList<Integer> list1=new ArrayList<>();
+// // //          ArrayList<Integer> list2=new ArrayList<>();
+// // //         list1.add(40);
+// // //         list2.add(5);
+// // //         list.addAll(list2);
+// // //         // list.add(1,40);
+// // //         // System.out.println(list);
+// // //        list.addAll(list1);
+// // //         // System.out.println(list1);
+// // //         in
+// // //     }
+    
+
+// // //         int first = list.get(0);
+// // //         int last =last.get(list.size()-1);
+// // //         System.out.println("first element:"+ first);
+// // //         System.out.println("last element:"+ last);
+
+// // //     //    list2 
+// // // list2=list;
+// // // System.out.println("list2:"+ list2);
+
+
+// LinkedList<Integer> list=new LinkedList<>();
+// list.add(5);
+// list.add(19);
+// list.add(7);
+// list.add(2,40);
+// System.out.println(list);
+//     }
+// }
+
+
+    
+
+
+
+
+
+
+
+
+
+// // size & Empty
+// int size=list.size();
+// boolean isEmpty= list.isEmpty();
+
+// //loop/iterate
+// //normal for loop
+// for(int i=0;i<list.size();i++){
+//     System.out.println(list.get(i));
+// }
+// //b)enhanced for loop
+// for(int sum :list){
+//     System.out.println(num);
+// }
+// //c) iterator
+
+
+
