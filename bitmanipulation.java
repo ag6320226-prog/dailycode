11f36f30ae215348dcdc655f62906461e7a359f6
Ayush Gupta <ag6320226@gmail.com>
2025-12-18 22:54:15 +0530

    bitmanipulation

---
+++ bitmanipulation.java
@@ -0,0 +1,55 @@
+// import java.util.Scanner;
+
+// public class bitmanipulation {
+//     public static void main(String[] args) {
+//         // get bit
+//         int n=4;
+//         int pos =3;
+//         int bitmask = 1<<pos;
+
+//         if((bitmask & n)==0){
+//             System.out.println("bit was zero:");
+//         }else{
+//             System.out.println("bit was one:");
+
+//         }
+//     }
+
+    
+// }
+// set bit
+// int n=5;
+// int pos=2;
+// int bitmask= 1<<pos;
+
+// int newNumber = bitmask | n;
+// System.out.println(newNumber);
+//     }
+// }
+//clear bit
+// int n=5;
+// int pos =2;
+// int bitmask= 1<<pos;
+// int notbitmask =~(bitmask);
+
+// int newNumber = notbitmask & n;
+// System.out.println(newNumber);
+//     }
+// }
+//update bit
+// Scanner sc= new Scanner(System.in);
+// int oper =sc.nextInt();
+// int n=5;
+// int pos=1;
+// int  bitmask=1<<pos;
+// if(oper==1){
+//     int newNumber=bitmask| n;
+//     System.out.println(newNumber);
+
+// }else{
+//     int newbitmask =~(bitmask);
+//     int newNumber = newbitmask&n;
+//     System.out.println(newNumber);
+// }
+//     }
+// }
