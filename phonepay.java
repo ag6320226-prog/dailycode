1612f8cc5b20847e3231610c2686867e15ace397
Ayush Gupta <ag6320226@gmail.com>
2025-12-18 22:56:14 +0530

    pay money

---
+++ phonepay.java
@@ -0,0 +1,65 @@
+// class PhonepayApp{
+//     private String username;
+//     private String UPID;
+//     private double balance;
+//     private String pin;
+//     private String accountNumber;
+
+//     public String getusername(){
+//         return username;
+//     }
+//     public void setusername(String name){
+//         username=name;
+//         System.out.println("username:"+ getusername());
+//     }
+//     public String getUPID(){
+//     return UPID;
+//     }
+//     public void setUPID(String U){
+//         UPID=U;
+//         System.out.println("UPID:"+ getUPID());
+//     }
+//     public double getbalance(){
+//         return balance;
+
+//     }
+//     public void setbalance(double b){
+//         balance=b;
+//         System.out.println("balance:"+ getbalance());
+//     }
+//     public String getpin(){
+//         return pin;
+//     }
+//     public void setpin(String p){
+//         pin= p;
+//                   System.out.println("pin:"+ getpin());
+//     }
+//     public String getaccountNumber(){
+//         return accountNumber;
+//     }
+//     public void setaccountNumber(String ac){
+//         accountNumber=ac;
+//                    System.out.println("accountNumber:"+ getaccountNumber());
+//     }
+//     public void password(){
+//         System.out.println("password:"+ 378276);
+//     }
+
+
+    
+  
+
+
+// public class phonepay{
+//     public static void main(String[] args) {
+//         phonepay p1=new phonepay();
+//         p1.password();
+//         p1.setusername("munnibai");
+//         p1.setUPID("9976346747@upi");
+//         p1.setbalance(10000);
+//         p1.setpin("600679");
+//         p1.setaccountNumber("405899788437852938");
+        
+//         }
+    // }
+
