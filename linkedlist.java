014dba30781d967364120bf3f0fcd8d5ab96b36f
Ayush Gupta <ag6320226@gmail.com>
2025-12-20 12:29:50 +0530

    singular

---
+++ linkedlist.java
@@ -0,0 +1,139 @@
+
+// import java.util.*;
+// public class linkedlist {
+//     // node head;
+//     // private int size;
+
+//     // linkedlist(){
+//     //     this.size=0;
+//     // }
+//     // class node{   //node ke andar variable store
+//     //     String data;
+//     //     node next;
+
+//     //     node(String data){   // constructor
+//     //         this.data=data;
+//     //         this.next = null;
+//     //         size++;
+
+//     //     }
+
+
+//     // }
+
+//     // public void addfirst(String data){
+//     //    node Newnode=new node(data);
+//     //    if(head==null){
+//     //       head = Newnode;
+//     //     return;
+//     //    }
+//     //    Newnode.next = head;
+//     //    head = Newnode;
+
+
+
+//     //     }
+//     //     public void addlast(String data){
+//     //             node Newnode=new node(data);
+//     //    if(head==null){
+//     //       head = Newnode;
+//     //     return;
+
+//     //     }
+//     //     node currNode = head;
+//     //     while(currNode.next != null){
+//     //         currNode= currNode.next;
+//     //     }
+//     //     currNode.next = Newnode;
+
+//     // }
+//     // public void printlist(){
+//     //     if(head == null){
+//     //         System.out.println("list is khali");
+//     //         return;
+//     //     }
+//     //     node currNode = head;
+//     //     while(currNode != null){
+//     //         System.out.print(currNode.data + "->");
+//     //         currNode= currNode.next;
+//     //     }
+//     //   System.out.println("null");
+//     // }
+//     // //delete first
+//     // public void delfirst(){
+//     //     if(head== null){
+//     //         System.out.println("the list is khali");
+//     //         return ;
+
+//     //     }
+//     //     size--;
+//     //     head =head.next;
+//     // }
+//     // public void delLast(){
+
+//     //     if(head==null){
+//     //         System.out.println("the list is empty");
+//     //         return;
+//     //     }
+//     //     size--;
+//     //     if(head.next== null){
+//     //         head= null;
+//     //         return;
+//     //     }
+//     //   node secondLast = head;
+//     //   node lastNode = head .next;
+//     //   while(lastNode.next!= null){
+//     //     lastNode =lastNode.next;
+//     //     secondLast = secondLast. next;
+
+//     //   }
+//     //   secondLast.next = null;
+
+//     // }
+//     // public int getsize(){
+//     //     return size;
+//     // }
+
+
+//     public static void main(String[] args) {
+//         LinkedList<String> list= new LinkedList<>();
+
+//         list.addFirst("a");
+//         list.addFirst("is");
+
+//         System.out.println(list);
+
+//         list.addLast("this");
+//         System.out.println(list);
+        // linkedlist ll=new linkedlist();
+        // ll.addfirst("a");
+        // ll.addfirst("is");
+        // ll.printlist();
+
+        // ll.addlast("list");
+        // ll.printlist();
+        // ll.addfirst("this");
+        // ll.printlist();
+
+        // ll.delfirst();
+        // ll.printlist();
+
+
+        // ll.delLast();
+        // ll.printlist();
+
+
+        //   System.out.println(ll.getsize());
+        //   ll.addlast("list");
+          
+        //   System.out.println(ll.getsize());
+
+          
+          
+
+
+
+        
+//     }
+    
+// }
