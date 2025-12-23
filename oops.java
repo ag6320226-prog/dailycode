55313158b8f41dee3971d82d238394fca8c7b604
Ayush Gupta <ag6320226@gmail.com>
2025-12-23 20:41:58 +0530

    ooops

---
+++ oops.java
@@ -0,0 +1,90 @@
+// class pen{
+//     String color;
+//     String type;
+
+//     public void write(){
+//         System.out.println("writing something");
+//     }
+//     public void printColor(){
+//         System.out.println(this.color);
+//     }
+// }
+//             class Student{
+//          String name;
+//            int age;
+
+//     public void printInfo(){
+//         System.out.println(this.name);
+//         System.out.println(this.age);
+//     }
+//     // Student(String name,int age){   //parameteries const.
+//     Student(Student s2){
+
+    
+//            this.name=s2.name;
+//              this.age= s2.age;
+//     }
+//     Student(){
+
+//     }
+// }
+
+// public class oops {
+
+
+//     public static void main(String[] args) {
+
+
+//         // Student s= new Student("khevdi",20); 
+//         Student s= new Student();
+        
+        
+//         s.name="Ram";
+//         s.age= 27;
+
+//         Student s2= new Student(s);
+        
+
+//         s.printInfo();
+        
+//     //     pen pen1=new pen();
+//     //     pen1.color ="red";
+//     //     pen1.type="gel";
+
+//     //     pen pen2= new pen();
+//     //     pen2.color="blue";
+//     //     pen2.type="ballpoin";
+
+//     //     pen1.printColor();
+//     //       pen2.printColor();
+//     // }
+    
+// }
+// }
+
+// class Student{
+//     String name;
+//     int age;
+
+//     public void printInfo(String nmae){
+//         System.out.println(name);
+//     }
+//      public void printInfo(int age){
+//         System.out.println(age);
+
+// }
+// public void printInfo(String name,int age){
+//     System.out.println(name+ " "+age);
+// }
+// }
+// public class oops{
+//     public static void main(String[] args) {
+//         Student s= new Student();
+//         s.name= "babulal";
+//         s.age= 37;
+
+//         s.printInfo(s.name,s.age);
+//     }
+// 
+
+
