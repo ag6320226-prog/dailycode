c91f11dba9db40680aa064cd4c318574e11a5f11
Ayush Gupta <ag6320226@gmail.com>
2025-12-24 19:18:48 +0530

    abs

---
+++ abstraction.java
@@ -0,0 +1,68 @@
+// abstract class Animal{
+//     abstract void walk();
+//     public void eat(){
+//         System.out.println("animal eats");
+
+//     }
+// }
+//     class Horse extends Animal{
+//         public void walk(){
+//             System.out.println("walks on 4 legs");
+//         }
+//     }
+//     class Chicken extends Animal{
+//         public void walk(){
+//             System.out.println("walks on 2 legs");
+//         }
+//     }
+
+
+
+// public class abstraction{
+//    public static void main(String[] args) {
+//     Horse h= new Horse();
+//     h.walk();
+//   h.eat();
+//    }
+
+// }
+    
+// interface Animal{
+//     int eyes =2;
+//     public void walk();
+
+//     }
+    
+//     class Horse implements Animal{
+//         public void walk(){
+//             System.out.println("walks on 4 legs");
+//         }
+//     }
+//     public class abstraction {
+//     public static void main(String[] args) {
+//         Horse h= new Horse();
+//         h.walk();
+//     }
+        
+    
+// // }
+// class Student{
+//     String name;
+//     static String school;
+
+//     public static void changeSchool(){
+//         school = "newschool";
+//     }
+// }
+// public class abstraction{
+//     public static void main(String[] args) {
+//         Student.school = "abc";
+//         Student.Student1= new Student();
+//         Student1.name ="tony";
+//         System.out.println(Student1.school);
+
+
+        
+//     }
+    
+// }
