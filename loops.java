a741574653a36ae18fe49363c77541ba64314e97
Ayush Gupta <ag6320226@gmail.com>
2025-12-24 21:01:26 +0530

    for,do while,while

---
+++ loops.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args) {
+
+
+
+        //  FOR LOOP
+
+//         // for(int counter =0; counter<10;counter++){
+//         //     System.out.println(counter +" ");
+//         for(int i=0;i<12;i++){
+//             System.out.println(i);
+//         }
+//     }
+    
+// }
+
+//while loop
+
+// int i=0;
+// while(i<10){
+//     System.out.println(i);
+//     i=i+1;
+// }
+//     }
+// }
+
+//DO while loop
+
+// int i=12;
+// while(i<11){
+//     System.out.println("apna cll");
+// }
+// do{
+//     System.out.println("apna cl");
+
+// }while(i<11);
+// }
+
+//     }
+
+//print the sum of first n natural no
+Scanner sc = new Scanner(System.in);
+int n= sc.nextInt();
+// int sum =0;
+// for(int i=1;i<=n;i++){
+//     sum= sum+i;
+// }
+// System.out.println(sum);
+//     }
+// }
+
+//print the table of a number input by the usr
+for(int i=1;i<11;i++){
+    System.out.println(i*n);
+}
+    }
+}
