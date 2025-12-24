f7f7ea4dc14bc3829a95cb7c7bbd76545b14b8b8
Ayush Gupta <ag6320226@gmail.com>
2025-12-24 19:19:42 +0530

    inh.

---
+++ inheritance.java
@@ -0,0 +1,31 @@
+// class shape{
+//     public void area(){
+//         System.out.println("displays area");
+//     }
+// }
+// class Triangle extends shape{
+//     public void area(int l,int h){
+//         System.out.println(1/2*l*h);
+//     }
+// }
+// class EquilateralTriangle extends Triangle{
+//       public void area(int l,int h){
+//         System.out.println(1/2*l*h);
+// }
+// }
+
+
+
+// public class inheritance {
+//     public static void main(String[] args) {
+//         // EquilateralTriangle et= new EquilateralTriangle();
+//         // et.area(5, 6);
+//         // et.area(3,6);
+
+    
+// }
+
+    
+// }
+
+
