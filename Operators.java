b6bf8014a6639a42f0eb07d39d91e30cbb390524
Ayush Gupta <ag6320226@gmail.com>
2025-12-25 18:05:29 +0530

    all operators

---
+++ Operators.java
@@ -0,0 +1,114 @@
+import java.util.Scanner;
+
+public class Operators {
+    public static void main(String[] args) {
+
+        //  preincrement
+
+    //     int a=10;
+    //     int b=0;
+    //     b=a++;
+    //     System.out.println(a);
+    //     System.out.println(b);
+
+
+    // }
+    
+    //postincrement
+    //  int a=10;
+    //  int b=0;
+
+    //  b=++a;
+    //  System.out.println(a);
+    //  System.out.println(b);
+    
+
+    //predecrement
+//     int a=19;
+//     int b=0;
+
+//     b=--a;
+//     System.out.println(a);
+//     System.out.println(b);
+// }
+
+// postdecrement
+// int a=10;
+// int b=0;
+
+// b=a--;
+// System.out.println(a);
+// System.out.println(b);
+//     }
+// }
+
+
+// relational operators
+//  Scanner sc= new Scanner(System.in);
+//  System.out.println("enter first number");
+ 
+// int x=sc.nextInt();
+
+// System.out.println("enter second number");
+// int y= sc.nextInt();
+
+// System.out.println("x>y :"+(x>y));
+
+// System.out.println("x<y :"+(x<y));
+
+// System.out.println("x>=y :"+(x>=y));
+// System.out.println("x<=y :"+(x<=y));
+
+// System.out.println("x==y :"+(x==y));
+
+// System.out.println("x!=y :"+(x!=y));
+//     }
+// }
+
+//  Logical operators
+//  Scanner sc= new Scanner(System.in);
+//  System.out.println("enter first number");
+ 
+// int x=sc.nextInt();
+
+// System.out.println("enter second number");
+// int y= sc.nextInt();
+
+
+// System.out.println("x>0 && y>0:" +(x>0 && y>0));
+
+// System.out.println("!(x>y):" +!(x>y));
+
+// System.out.println("x>0 || y>0:" +(x>0 || y>0));
+
+
+//     }
+// }
+
+// decimal to Binary
+//  Scanner sc= new Scanner(System.in);
+//  System.out.println("Enter a decimal number:");
+//  int num= sc.nextInt();
+
+
+//  int binary[]= new int[32];
+//  int i=0;
+
+//  while(num>0){
+//     binary[i]= num %2;
+//     num = num/2;
+//     i++;
+
+//  }
+//  System.out.println("Binary equivalent");
+//  for(int j=i-1;j>=0;j--){
+//     System.out.println(binary[j]);
+
+//  }
+//     }
+//  }
+
+
+
+
+
