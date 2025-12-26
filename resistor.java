fed23f2a81bf0a21b5092f66e684c45bbf973c37
Ayush Gupta <ag6320226@gmail.com>
2025-12-26 23:27:34 +0530

    resis

---
+++ resistor.java
@@ -0,0 +1,22 @@
+// import java.util.HashMap;
+// public class resistor {
+
+
+
+//     public static void main(String[] args) {
+//         String colors[]={"black","brown","red","orange","green","blue","voilet","grey","white"};
+//         HashMap<String,Integer>map=new HashMap<>();
+//         for(int i=0;i<colors.length;i++){
+//             map.put(colors[i],i);
+
+//         }
+//         String col1="red";
+//         String col2="green";
+//         int v1=map.get(col1);
+//         int v2=map.get(col2);
+//         int value=v1*10+v2;
+//         System.out.println(value);
+
+// }
+// }
+// 
