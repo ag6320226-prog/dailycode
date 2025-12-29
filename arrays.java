29badbd07797f949e8c4ac42b4ab5aa914670eeb
Ayush Gupta <ag6320226@gmail.com>
2025-12-29 21:43:03 +0530

    arr

---
+++ arrays.java
@@ -0,0 +1,64 @@
+// import java.util.*;
+// public class arrays {
+//     public static void main(String[] args) {
+//         Scanner sc= new Scanner(System.in);
+//         int size =sc.nextInt();
+//         int numbers[]= new int[size];
+
+//         for(int i=0;i<size;i++)
+// {
+//     numbers[i]=sc.nextInt();
+
+// }
+// int x=sc.nextInt();
+
+// for(int i=0;i<numbers.length;i++){
+//     if(numbers[i]==x){}
+//     System.out.println("found at index:" +i);
+// }
+//     }
+// }
+// int marks[]=new int[4];
+// marks[0]=93;
+// marks[1]=48;
+// marks[2]=94;
+// marks[3]=67;
+
+// for(int i=0;i<4;i++){
+//     System.out.println(marks[i]);
+// }
+//     }
+// }
+//sort numeric and string array
+// int arr[]={4,6,2,8,5,0};
+// Arrays.sort(arr);
+// System.out.println("sorted numeric array:"+Arrays.toString(arr));
+//     }
+// }
+//sum all values in an arr
+// int arr[]={3,5,2,4,5,6,8,0};
+// int sum=0;
+// for(int i=0;i<arr.length;i++){
+//     sum+=arr[i];
+
+// }
+// System.out.println("sum of all elements:"+sum);
+//     }
+// }
+//print a 10*10 grid of dashes
+// int row=10;
+// int cols=10;
+
+// for(int i=0;i<row;i++){
+//     for(int j=0;j<cols;j++){
+//         System.out.print("-");
+//     }
+// System.out.println();
+// }
+//     }
+// }
+
+
+
+
+
