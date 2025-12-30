0f5009e64b7517ce2a1186aaede89fcdf71e3686
Ayush Gupta <ag6320226@gmail.com>
2025-12-30 22:26:08 +0530

    snap

---
+++ snapchat.java
@@ -0,0 +1,52 @@
+// public class snapchat {
+ 
+//     private String recipient;
+//     private String content; 
+//     private int duration;
+
+//     public void setRecipient(String recipient) {
+//         this.recipient = recipient;
+//     }
+
+//     public void setContent(String content) {
+//         if (content != null && !content.isEmpty()) {
+//             this.content = content;
+//         }
+//     }
+
+//     public void setDuration(int duration) {
+//         if (duration > 0 && duration <= 10) {
+//             this.duration = duration;
+//         }
+//     }
+//     public String getRecipient() {
+//         return recipient;
+//     }
+//     public String getContent() {
+//         return content;
+//     }
+//     public int getDuration() {
+//         return duration;
+//     }
+//     public void sensnap(){
+//         if(recipient != null && content != null && duration >0){
+//             System.out.println("snapent to"+ recipient + "with content :"+ content+ " for"+duration+"seconds.");
+//         }else{
+//             System.out.println("snap cannot be sent please ensure all fields are se correctly.");
+//         }
+//     }
+//     public static void main(String[] args) {
+//         snapchat s= new snapchat();
+//         s.setRecipient("john_doe");
+//         s.setContent("hello,this is a snap!");
+//         s.setDuration(5);
+//         s.sensnap();
+//     }
+
+
+
+
+// }
+
+    
+
