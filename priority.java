920e7a9148edb5bacb0432bf20b81bfbd01db158
Ayush Gupta <ag6320226@gmail.com>
2025-12-31 21:06:32 +0530

    Queue

---
+++ priority.java
@@ -0,0 +1,100 @@
+// import java.util.PriorityQueue;
+
+// public class priority {
+//     public static void main(String[] args) {
+//         PriorityQueue<String> queue = new PriorityQueue<>();
+
+
+//        // Count PriorityQueue Elements
+//          queue.add("blue");
+   
+//           queue.add("black");
+//         queue.add("pink");
+//           queue.add("redchatak");
+//         queue.add("sky");
+//          System.out.println("original Priority Queue: "+queue);
+//          System.out.println("size count priorityqueue:"+queue.size());
+//     }
+
+
+
+
+
+      //  Clear PriorityQueue
+
+//       queue1.add("blue");
+//       queue1.add("brown");
+//  queue1.add("black");
+//   queue1.add("darkblue");
+//       queue1.add("yellow");
+//  queue1.add("pink");
+//  System.out.println("orignal priority queue:"+queue1);
+//  queue1.clear();
+//  System.out.println("the new priority queue:"+ queue1);
+//     }
+
+
+
+
+        //insert queue
+//         queue1.add("white");
+
+//         queue1.add("red");
+// queue1.add("black");
+// queue1.add("brown");
+// queue1.add("lighblue");
+// System.out.println("originalpriority queue:"+ queue1);
+// queue1.offer("pink");
+
+// System.out.println("the new priority queue:"+queue1);
+//     }
+
+
+// //Add All Elements to Another PriorityQueue
+// queue1.add("red");
+// queue1.add("black");
+// queue1.add("brown");
+// queue1.add("lighblue");
+
+// System.out.println("priority queue1:"+ queue1);
+
+// PriorityQueue<String>queue2= new PriorityQueue<>();
+// queue2.add("blue");
+// queue2.add("pink");
+// queue2.add("yello");
+// queue2.add("darkred");
+
+// System.out.println("priority queue2:"+queue2);
+// queue1.addAll(queue2
+// );
+// System.out.println("new priority queue:"+queue1);
+//     }
+
+
+
+
+        // Iterate PriorityQueue Elements
+        //   queue.add("blue");
+        //   queue.add("black");
+        // queue.add("pink");
+        //   queue.add("redchatak");
+        // queue.add("sky");
+        //  System.out.println("Elements of the Priority Queue: ");
+
+        //  for(String element: queue){
+        //     System.out.println(element);
+        //  }
+        //  }
+
+
+
+        //create and print queu
+
+//         queue.add("red");
+//         queue.add("blue");
+ //   queue.add("black");
+        // queue.add("pink");
+        //   queue.add("redchatak");
+        // queue.add("sky");
+//
+//}
