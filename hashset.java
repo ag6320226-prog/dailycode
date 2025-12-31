30c872c338590f228bd7033903422bc1a572634a
Ayush Gupta <ag6320226@gmail.com>
2025-12-31 19:58:55 +0530

    hashset

---
+++ hashset.java
@@ -0,0 +1,87 @@
+
+// public class hashset {
+
+//     public static void main(String[] args) {
+//         java.util.HashSet<String> set = new java.util.HashSet<>();
+//         //Check if HashSet is Empty
+//          set.add("red");
+//         set.add("blue");
+//           set.add("black");
+//         set.add("yellow");
+//           set.add("brown");
+//         set.add("pink");
+//           set.add("green");
+//         set.add("orange");
+
+//          System.out.println("Original Hash Set: " +set);
+//     System.out.println("Checking the above array list is empty or not! "+set.isEmpty());
+//     System.out.println("Remove all the elements from a Hash Set: ");
+//     set.removeAll(set);
+//     System.out.println("Hash Set after removing all the elements "+set);
+
+//         }
+
+
+
+//clear hashset
+
+//  set.add("red");
+//         set.add("blue");
+//           set.add("black");
+//         set.add("yellow");
+//           set.add("brown");
+//         set.add("pink");
+//           set.add("green");
+//         set.add("orange");
+
+//             System.out.println("Original Hash Set: " + set);
+//             set.removeAll(set);
+//  System.out.println("Hash Set after removing all the elements"+ set);
+//     }
+//     //     set.add("red");
+//     //     set.add("blue");
+    //       set.add("black");
+    //     set.add("yellow");
+    //       set.add("brown");
+    //     set.add("pink");
+    //       set.add("green");
+    //     set.add("orange");
+
+    //         System.out.println("Original Hash Set: " + set);
+    // System.out.println("Size of the Hash Set: " + set.size());}
+
+
+
+
+
+         //Iterate HashSet Elements
+    //      set.add("Apple");
+    //     set.add("Banana");
+    //     set.add("Orange");
+
+    //     // Displaying the HashSet
+    //     System.out.println("HashSet is:");
+    //      for(String element: set){
+    //         System.out.println(element);
+    //      }
+    // }
+
+
+
+
+
+//Append Element to HashSet
+    //     // Adding elements to the HashSet
+    //     set.add("Apple");
+    //     set.add("Banana");
+    //     set.add("Orange");
+
+    //     // Displaying the HashSet
+    //     System.out.println("HashSet: " + set);
+    // }
+
+
+    
+
+    
+// }
