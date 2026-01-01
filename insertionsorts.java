87426b9921304ac79f019396f24a51eb6d827be4
Ayush Gupta <ag6320226@gmail.com>
2026-01-01 20:41:29 +0530

    sort

---
+++ insertionsorts.java
@@ -0,0 +1,27 @@
+// public class insertionsorts {
+//     void sort(int a[]){
+//         int n = a.length;
+//         for(int i=1;i<n;++i){
+//             int k= a[i];
+//             int j=i-1;
+
+
+//             while(j >= 0&& a[j]>k){
+//                 a[j+1]= a[j];
+//                 j=j-1;
+//             }
+//             a[j+1]=k;
+//         }
+//     }
+//     public static void main(String[] args) {
+//         int a[] ={5,2,1,81,7,68,55,5,4,4,3,22,23};
+//         insertionsorts in= new insertionsorts();
+//         in.sort(a);
+//         int n = a.length;
+//         for(int i=0;i<n;++i){
+//             System.out.println(a[i] + " ");
+//         }
+//     }
+
+    
+// }
