eff736746d3993f1080c84b6225b444fbd9816ea
Ayush Gupta <ag6320226@gmail.com>
2026-01-01 21:05:41 +0530

    arr

---
+++ occurrences.java
@@ -0,0 +1,26 @@
+// import java.util.Arrays;
+
+// public class occurrences {
+//     public static int[] removeElements(int [] a,int k){
+//         int ind =0;
+
+//         for(int i=0;i<a.length;i++)
+//             if(a[i]!=k)
+//                 a[ind++] = a[i];
+            
+//                 return Arrays.copyOf(a, ind);
+
+//             }
+//             public static void main(String[] args) {
+//                 int[] a= {6,5,3,8,2,87,23,53,23,69};
+//                 int k=3;
+//                 a = removeElements(a, k);
+//                 System.out.println(Arrays.toString(a));
+
+//             }
+
+            
+//         }
+    
+    
+
