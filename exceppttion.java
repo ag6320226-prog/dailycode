53bad287f6f57c0a87836e2178f2146ca5893aa6
Ayush Gupta <ag6320226@gmail.com>
2026-01-01 20:40:15 +0530

    exception

---
+++ exceppttion.java
@@ -0,0 +1,25 @@
+public class exceppttion {
+    
+
+
+
+    //remmove leading zero
+//     public static String removeZero(String str){
+//         int i=0;
+//         while(i<str.length()&& str.charAt(i)=='0')
+//             i++;
+
+//         StringBuffer sb=new StringBuffer(str);
+
+//         sb.replace(0,i,"");
+//         return sb.toString();
+//     }
+//     public static void main(String[] args) {
+//         String str="000000389289932";
+
+//         str = removeZero(str);
+//         System.out.println(str);
+//     }
+
+    
+// }
