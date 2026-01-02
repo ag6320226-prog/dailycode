3909c5d64f094f6d2ac02d70a9dc3086025198a3
Ayush Gupta <ag6320226@gmail.com>
2026-01-02 23:18:31 +0530

    short

---
+++ heap.java
@@ -0,0 +1,37 @@
+// public class heap {
+//     static void heapify (int[] arr,int n,int i){
+//         int largest =i;
+//         int l = 2*i +1;
+//         int r = 2* i+2;
+
+//         if(l<n && arr[l]> arr[largest])
+//             largest =l;
+
+//         if(largest !=i){
+//             int temp = arr[i];
+//             arr[i] = arr[largest];
+//             arr[largest] =temp;
+          
+//             heapify(arr, n, largest);
+//         }
+//     }
+//     static void heapSort(int[] arr){
+//         int n = arr.length;
+
+//         for(int i = n-1;i>0;i--){
+//             int temp =arr[0];
+//             arr[0]= arr[i];
+//             arr[i]= temp;
+
+//             heapify(arr, i, 0);
+//         }
+//     }
+// public static void main(String[] args) {
+//     int arr[]= {9,6,4,2,7,5,0,34,67,14,39};
+//     heapSort(arr);
+//     for(int i=0;i<arr.length;++i){
+//         System.out.println(arr[i] +" ");
+//     }
+// }
+    
+// }
