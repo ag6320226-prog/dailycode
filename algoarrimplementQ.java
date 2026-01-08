5ddfdee1f2fb656967affe5c3ac39465aae6f508
Ayush Gupta <ag6320226@gmail.com>
2026-01-08 13:13:19 +0530

    algoarrimpQuqe

---
+++ algoarrimplementQ.java
@@ -0,0 +1,74 @@
+// import java.util.*;
+// // array implementation of Queue
+
+// public class algoarrimplementQ {
+//     static class Queue{
+//         static int arr[];
+//         static int size;
+//         static int rear;
+
+
+//         Queue(int n){
+//             arr = new int[n];
+//             size = n;
+//             rear= -1;
+//         }
+//         public static boolean isEmpty(){
+//             return rear == -1;
+
+//         }
+
+//         // add
+//         public static void add(int data){
+//             if(rear == size-1){
+//                 System.out.println("queue is full");
+//                 return;
+
+//             }
+//             rear++;
+//             arr[rear] = data;
+//         }
+//         public static int remove(){
+//             if(isEmpty()){
+//                 System.out.println("queue is empty");
+//                 return -1;
+//             }
+//             int front = arr[0];
+//             for(int i=0;i<rear;i++){
+//                 arr[i] = arr[i+1];
+//             }
+//             rear--;
+//             return front;
+//         }
+//         //peak
+//         public static int peek(){
+//             if(isEmpty()){
+//                 System.out.println("Queue is empty");
+//                 return -1;
+
+//             }
+//             return arr[0];
+//         }
+//     }
+//     public static void main(String[] args) {
+//         Queue q= new Queue(7);
+//         q.add(1);
+//          q.add(5);
+//             q.add(2);
+//          q.add(4);
+//             q.add(0);
+//          q.add(8);
+//            q.add(1);
+//          while (!q.isEmpty()) {
+//             System.out.println(q.peek());
+
+//             q.remove();
+//          }
+//     }
+
+
+
+
+
+    
+// }
