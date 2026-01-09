c1e2408dd0f0e79d486f55cd5c3ba0df8fb171c9
Ayush Gupta <ag6320226@gmail.com>
2026-01-09 19:51:17 +0530

    hashing

---
+++ duplicateKdis.java
@@ -0,0 +1,24 @@
+// import java.util.*;
+// //duplicate within K distances in an array
+// public class duplicateKdis {
+//     static boolean checkDuplicatesWithinK(int[] arr , int k){
+//         int n = arr.length;
+//         for(int i=0;i<n;i++){
+//             for(int c =  1;c <= k&&(i + c) < n;c++){
+//                 int j=  i+c;
+
+//                 if(arr[i] == arr[j])
+//                     return true;
+//             }
+
+//         }
+//         return false;
+//     }
+//     public static void main(String[] args) {
+//         int[] arr = {10,5,3,48,22,3,19};
+//         System.out.println(checkDuplicatesWithinK(arr, 3)? "yes" : "No");
+//     }
+
+
+    
+// }
