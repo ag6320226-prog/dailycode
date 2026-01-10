a6a5716960be187a60623c6620943f7de879b46d
Ayush Gupta <ag6320226@gmail.com>
2026-01-10 21:13:00 +0530

    fizzbuzz

---
+++ fizzBuzz.java
@@ -0,0 +1,32 @@
+
+// import java.util.*;
+// public class fizzBuzz {
+//     static ArrayList<String>FizzBuzz(int n){
+
+//         ArrayList<String> res = new ArrayList<>();
+//         for(int i=1;i<= n;++i){
+
+//             if(i % 3 == 0 && i% 5 == 0){
+//                 res.add("FizzBuzz");
+//             }
+
+//             // check if i is divisible by 3
+//             else if(i%3==0){
+//                 res.add("Buzz");
+//             }else{
+//                 res.add(Integer.toString(i));
+//             }
+//         }
+//         return res;
+
+//     }
+//     public static void main(String[] args) {
+//         int n = 24 ;
+//         ArrayList<String> res = FizzBuzz(n);
+//         for(String s : res){
+//             System.out.println(s + " ");
+//         }
+//     }
+
+    
+// }
