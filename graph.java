605fd3b6b824b1b065384deb14c0bd0b91bf5f97
Ayush Gupta <ag6320226@gmail.com>
2026-01-15 20:52:48 +0530

    graph

---
+++ graph.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class graph {
+    static ArrayList<ArrayList<Integer>> createGraph(int V,int[][] edges){
+        ArrayList<ArrayList<Integer>> mat= new ArrayList<>();
+
+        for(int i=0;i<V;i++){
+            ArrayList<Integer>row = new ArrayList<>(Collections.nCopies(V, 0));
+            mat.add(row);
+
+        }
+        for(int[] it: edges){
+            int u= it[0];
+            int v= it[1];
+            mat.get(u).set(v,1);
+
+            mat.get(v).set(u, 1);
+        }
+        return mat;
+
+    }
+    public static void main(String[] args) {
+        int V =3;
+
+        int[][] edges ={{0,1},{0,2},{1,2}};
+
+        ArrayList<ArrayList<Integer>> mat= createGraph(V, edges);
+
+        System.out.println("adjacency Matrix Represetation:");
+
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                System.out.print(mat.get(i).get(j) + " ");
+                System.out.println();
+            }
+        }
+    }
+    
+}
